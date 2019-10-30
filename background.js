d63a6f2970c36f71816e658468b25f6713685197
jasmaa <jasonmaa3955@gmail.com>
2019-10-30 14:33:35 -0400

    Fix for chrome

--- background.js
+++ background.js
@@ -1,4 +1,27 @@
 
-chrome.webNavigation.onHistoryStateUpdated.addListener(function (details) {
-    chrome.tabs.sendMessage(details.tabId, details.url);
-});
+const API_KEY = "<api key>";
+
+/**
+ * Retrieve comments
+ * @param {*} apiKey 
+ * @param {*} videoId 
+ */
+async function getComments(apiKey, videoId) {
+    const resp = await fetch(`https://www.googleapis.com/youtube/v3/commentThreads?key=${apiKey}&textFormat=plainText&part=snippet&videoId=${videoId}&maxResults=10`);
+    const data = await resp.json();
+
+    return data['items'].map(x => x['snippet']['topLevelComment']['snippet']['textDisplay']);
+}
+
+function updateCommentHandler(details) {
+    const VIDEO_ID = details.url.match(/v=(.*)/)[1];
+    getComments(API_KEY, VIDEO_ID)
+        .then(commentTexts => {
+            console.log(commentTexts);
+            chrome.tabs.sendMessage(details.tabId, commentTexts);
+        });
+}
+
+chrome.webNavigation.onHistoryStateUpdated.addListener(updateCommentHandler);
+chrome.webNavigation.onCompleted.addListener(updateCommentHandler);
+
--- content.js
+++ content.js
@@ -1,8 +1,6 @@
 
-const API_KEY = "<api key>";
 const FONT_SIZE = 12;
 let comments = [];
-let videoId = window.location.toString().match(/v=(.*)/)[1];
 
 // Add canvas to video player
 const player = document.getElementById("movie_player").getElementsByClassName("html5-video-container")[0];
@@ -14,44 +12,18 @@ const ctx = canvas.getContext('2d');
 ctx.fillStyle = 'white';
 ctx.font = `${FONT_SIZE}px Arial`;
 
-/**
- * Retrieve comments
- * @param {*} apiKey 
- * @param {*} videoId 
- */
-async function getComments(apiKey, videoId) {
-	const resp = await fetch(`https://www.googleapis.com/youtube/v3/commentThreads?key=${apiKey}&textFormat=plainText&part=snippet&videoId=${videoId}&maxResults=10`);
-	const data = await resp.json();
-
-	return data['items'].map(x => x['snippet']['topLevelComment']['snippet']['textDisplay']);
-}
-
-/**
- * Load comments
- * @param {string} videoId 
- */
-function load(videoId) {
-	getComments(API_KEY, videoId)
-		.then(commentTexts => {
-			comments = [];
-			for (let v of commentTexts) {
-				comments.push({
-					x: Math.random() * canvas.width + canvas.width,
-					y: Math.random() * (canvas.height - FONT_SIZE) + FONT_SIZE,
-					speed: Math.random() * 3 + 1,
-					text: v,
-				});
-			}
-		});
-}
-
 // Load comments
 chrome.runtime.onMessage.addListener(function (data, sender) {
-	videoId = data.match(/v=(.*)/)[1];
-	load(videoId);
+	comments = [];
+	for (let v of data) {
+		comments.push({
+			x: Math.random() * canvas.width + canvas.width,
+			y: Math.random() * (canvas.height - FONT_SIZE) + FONT_SIZE,
+			speed: Math.random() * 3 + 1,
+			text: v,
+		});
+	}
 });
-load(videoId);
-
 
 // Animate comments
 let elapsed = 0;
