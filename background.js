8858f59187be17750b81235d1e27d820f3cc6352
jasmaa <jasonmaa3955@gmail.com>
2023-10-07 13:49:04 -0400

    Refactor and render comments based on video timestamp

--- background.js
+++ background.js
@@ -1,87 +1,43 @@
-
-const API_KEY = "<api key>";
-let videoTabs = {};
+const API_KEY = "<REPLACE ME>";
 
 /**
  * Fetches set of comments from video
- * @param {string} tabId Youtube video id
- * @param {object} param1 Video object
  */
-async function fetchComments(tabId, { videoId, nextPageToken }) {
-    const resp = await fetch(`https://www.googleapis.com/youtube/v3/commentThreads?key=${API_KEY}&textFormat=plainText&part=snippet&videoId=${videoId}&maxResults=10&pageToken=${nextPageToken}`);
+async function fetchComments(videoId, pageToken) {
+    if (pageToken === undefined) {
+        return {
+            commentTexts: [],
+        }
+    }
+
+    const url = new URL("https://www.googleapis.com/youtube/v3/commentThreads");
+    url.searchParams.append("key", API_KEY);
+    url.searchParams.append("textFormat", "plainText");
+    url.searchParams.append("part", "snippet");
+    url.searchParams.append("videoId", videoId);
+    url.searchParams.append("maxResults", 100);
+    if (pageToken) {
+        url.searchParams.append("pageToken", pageToken);
+    }
+
+    const resp = await fetch(url);
     const data = await resp.json();
 
-    const token = data['nextPageToken'] == undefined ? "" : data['nextPageToken'];
-    videoTabs[tabId] = { videoId: videoId, nextPageToken: token };
-    return data['items'].map(x => x['snippet']['topLevelComment']['snippet']['textDisplay']);
+    const nextPageToken = data['nextPageToken'];
+    const commentTexts = data['items'].map(x => x['snippet']['topLevelComment']['snippet']['textDisplay']);
+    return {
+        commentTexts,
+        nextPageToken,
+    };
 }
 
-/**
- * Gets video id from url
- * @param {string} url 
- */
-function getVideoId(url) {
-    if (url.match(/youtube\.com\/watch/)) {
-        const videoId = url.match(/v=(.*)/)
-        if (videoId) {
-            return videoId[1]
-        }
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.id === "fetch-comments") {
+        (async () => {
+            const { videoId, pageToken } = message.args;
+            const res = await fetchComments(videoId, pageToken);
+            sendResponse(res);
+        })();
+        return true;
     }
-    return null;
-}
-
-/**
- * Updates content script with new comments
- * 
- * @param {string} tabId 
- * @param {object} video 
- * @param {object} options 
- */
-function updateComments(tabId, video, options = { reset: false }) {
-    fetchComments(tabId, video)
-        .then(commentTexts => {
-            chrome.tabs.sendMessage(parseInt(tabId), {
-                commentTexts: commentTexts,
-                options: options,
-            });
-        });
-}
-
-/**
- * Initializes video comments
- * 
- * @param {object} details 
- */
-function initVideoHandler(details) {
-    const videoId = getVideoId(details.url);
-    if (videoId) {
-        videoTabs[details.tabId] = { videoId: videoId, nextPageToken: "" };
-        updateComments(details.tabId, videoTabs[details.tabId], { reset: true });
-    }
-}
-
-/**
- * Updates video comments
- */
-function updateHandler() {
-    for (const tabId in videoTabs) {
-        updateComments(tabId, videoTabs[tabId]);
-    }
-}
-
-
-chrome.tabs.query({currentWindow: true}, tabs => {
-    for (const tab of tabs) {
-        const videoId = getVideoId(tab.url);
-        if (videoId) {
-            videoTabs[tab.id] = { videoId: videoId, nextPageToken: "" };
-            updateComments(tab.id, videoTabs[tab.id], { reset: true });
-        }
-    }
-});
-
-setInterval(updateHandler, 30_000);
-
-chrome.tabs.onRemoved.addListener((tabId, _) => { delete videoTabs[tabId] });
-chrome.webNavigation.onCompleted.addListener(initVideoHandler);
-chrome.webNavigation.onHistoryStateUpdated.addListener(initVideoHandler);
+})
