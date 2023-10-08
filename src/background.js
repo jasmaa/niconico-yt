c164cb8bdc3017b98afb954768bdd83fccc3f3d7
jasmaa <jasonmaa3955@gmail.com>
2023-10-08 09:23:05 -0400

    Add webpack build so I stop accidentally pasting the API key into source control

--- src/background.js
+++ src/background.js
@@ -1,4 +1,4 @@
-const API_KEY = "<REPLACE ME>";
+const API_KEY = process.env.API_KEY;
 
 /**
  * Fetches set of comments from video
---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require("path");
+const webpack = require("webpack");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+require("dotenv").config();
+
+const stylesHandler = "style-loader";
+const { version } = require('./package.json');
+
+module.exports = (env) => {
+  const isProduction = env.NODE_ENV == "production";
+
+  const config = {
+    entry: {
+      content: "./src/content",
+      background: "./src/background.js",
+    },
+    output: {
+      path: path.resolve(__dirname, "dist"),
+    },
+    devServer: {
+      open: true,
+      host: "localhost",
+    },
+    plugins: [
+      // Add your plugins here
+      // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+
+      new CopyPlugin({
+        patterns: [
+          { from: "./src/icons", to: "./icons" },
+          {
+            from: `./src/manifest.json`,
+            to: "./manifest.json",
+            transform(content, path) {
+              const manifest = JSON.parse(content);
+              manifest["version"] = version;
+              return JSON.stringify(manifest, null, 2);
+            },
+          },
+        ],
+      }),
+      new webpack.EnvironmentPlugin({
+        API_KEY: process.env.API_KEY,
+      }),
+      new CleanWebpackPlugin(),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/i,
+          exclude: ["/node_modules/"],
+          loader: "babel-loader",
+        },
+        {
+          test: /\.css$/i,
+          use: [stylesHandler, "css-loader", "postcss-loader"],
+        },
+        {
+          test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+          type: "asset",
+        },
+
+        // Add your rules for custom modules here
+        // Learn more about loaders from https://webpack.js.org/loaders/
+      ],
+    },
+    resolve: {
+      extensions: [".jsx", ".js", "..."],
+    },
+    optimization: {
+      minimize: false,
+    },
+  };
+
+  if (isProduction) {
+    config.mode = "production";
+  } else {
+    config.mode = "development";
+  }
+
+  return config;
+};
