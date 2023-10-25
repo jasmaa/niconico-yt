49ee84a656f3585111396614749096c2804d5d58
jasmaa <jasonmaa3955@gmail.com>
2023-10-25 09:31:36 -0400

    Add generic to TimestampBucketMap. Add types for messaging.

--- src/messaging.ts
+++ src/messaging.ts
@@ -1,6 +1,52 @@
+import { CommentSettings } from "./settings";
+
 export enum Message {
   FETCH_COMMENTS = "fetch-comments",
   GET_SETTINGS = "get-settings",
   MERGE_SETTINGS = "merge-settings",
   SET_SETTINGS = "set-settings",
+}
+
+export interface FetchCommentsRequest {
+  id: Message.FETCH_COMMENTS;
+  args: {
+    videoId: string;
+    pageToken: string;
+  };
+}
+
+export interface FetchCommentsResponse {
+  status: number;
+  commentTexts: string[];
+  nextPageToken?: string;
+}
+
+export interface GetSettingsRequest {
+  id: Message.GET_SETTINGS;
+}
+
+export interface GetSettingsResponse {
+  settings: CommentSettings;
+}
+
+export interface MergeSettingsRequest {
+  id: Message.MERGE_SETTINGS;
+  args: {
+    settings: Partial<CommentSettings>;
+  };
+}
+
+export interface MergeSettingsResponse {
+  settings: CommentSettings;
+}
+
+export interface SetSettingsRequest {
+  id: Message.SET_SETTINGS;
+  args: {
+    settings: CommentSettings;
+  };
+}
+
+export interface SetSettingsResponse {
+  settings: CommentSettings;
 }
