c8f1dd807475b5b5cf3a2699bacc78af5a5d13e8
jasmaa <jasonmaa3955@gmail.com>
2024-08-26 22:36:50 -0400

    fix: Use local storage for settings
    Fixes #5

--- src/entrypoints/background.ts
+++ src/entrypoints/background.ts
@@ -11,14 +11,16 @@ import { fetchComments } from "../youtube";
 
 const API_KEY = process.env.API_KEY;
 
-let settings: Settings = {
+const DEFAULT_SETTINGS: Settings = {
   commentsVisible: true,
   commentOpacity: OpacityLevel.HIGH,
   commentSpeed: SpeedLevel.HIGH,
 };
 
+let settings: Settings;
+
 /**
- * Syncs settings to all content scripts
+ * Syncs settings to all content scripts and storage
  */
 function syncSettings() {
   const req: SetSettingsRequest = {
@@ -32,31 +34,52 @@ function syncSettings() {
       chrome.tabs.sendMessage(tabs[i].id, req);
     }
   });
+  chrome.storage.local.set({
+    settings: settings,
+  });
 }
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.id === Message.FETCH_COMMENTS) {
-    (async () => {
-      const req = message as FetchCommentsRequest;
-      const { videoId, pageToken } = req.args;
-      const res = await fetchComments(API_KEY, videoId, pageToken);
+/**
+ * Load settings from local storage
+ */
+async function loadSettings() {
+  const res = (await new Promise((resolve, reject) => {
+    chrome.storage.local.get("settings", (res) => {
+      resolve(res);
+    });
+  })) as any;
+  settings = res.settings
+    ? (res.settings as Settings)
+    : { ...DEFAULT_SETTINGS };
+}
+
+(async () => {
+  await loadSettings();
+
+  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.id === Message.FETCH_COMMENTS) {
+      (async () => {
+        const req = message as FetchCommentsRequest;
+        const { videoId, pageToken } = req.args;
+        const res = await fetchComments(API_KEY, videoId, pageToken);
+        sendResponse(res);
+      })();
+      return true;
+    } else if (message.id === Message.GET_SETTINGS) {
+      const res: GetSettingsResponse = {
+        settings,
+      };
       sendResponse(res);
-    })();
-    return true;
-  } else if (message.id === Message.GET_SETTINGS) {
-    const res: GetSettingsResponse = {
-      settings,
-    };
-    sendResponse(res);
-    return true;
-  } else if (message.id === Message.MERGE_SETTINGS) {
-    const req = message as MergeSettingsRequest;
-    settings = { ...settings, ...req.args.settings };
-    syncSettings();
-    const res: MergeSettingsResponse = {
-      settings,
-    };
-    sendResponse(res);
-    return true;
-  }
-});
+      return true;
+    } else if (message.id === Message.MERGE_SETTINGS) {
+      const req = message as MergeSettingsRequest;
+      settings = { ...settings, ...req.args.settings };
+      syncSettings();
+      const res: MergeSettingsResponse = {
+        settings,
+      };
+      sendResponse(res);
+      return true;
+    }
+  });
+})();
